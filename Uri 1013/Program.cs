ad21e291ff408c379a6fd9b4ca4b43e765aa1bd5
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-14 18:49:54 -0300

    Add files via upload

---
+++ Uri 1013/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Uri_1013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, v, m;
+
+
+
+            Console.WriteLine();
+            Console.Write("Digite 3 valores para saber qual é o maior : ");
+            string[] vetor = Console.ReadLine().Split(' ');
+            a = int.Parse(vetor[0]);
+            b = int.Parse(vetor[1]);
+            c = int.Parse(vetor[2]);
+
+            v = (a + b + Math.Abs(a-b)) / 2;
+            m = (v + c + Math.Abs(v - c)) / 2;
+
+            Console.WriteLine();
+            Console.WriteLine(m+" É o maior");
+            Console.WriteLine();
+
+        }
+    }
+}
